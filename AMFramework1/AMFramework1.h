5c3086e5b2e75ffb70c3a833d4fba9cce8947fb2
amrendra-roy <amar.roy17@gmail.com>
2021-01-05 15:58:21 +0530

    Initial Commit

---
+++ AMFramework1/AMFramework1.h
@@ -0,0 +1,18 @@
+//
+//  AMFramework1.h
+//  AMFramework1
+//
+//  Created by Amrendra Roy on 05/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AMFramework1.
+FOUNDATION_EXPORT double AMFramework1VersionNumber;
+
+//! Project version string for AMFramework1.
+FOUNDATION_EXPORT const unsigned char AMFramework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMFramework1/PublicHeader.h>
+
+
