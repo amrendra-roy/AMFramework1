e3424e718b1f3c184733a45b8fc5d115ceae812b
amrendra-roy <amar.roy17@gmail.com>
2021-01-05 17:48:10 +0530

    - Adding workspace with client app and frameworks

---
+++ AMFramework1/FrameOne.swift
@@ -0,0 +1,25 @@
+//
+//  FrameOne.swift
+//  AMFramework1
+//
+//  Created by Amrendra Roy on 05/01/21.
+//
+
+import Foundation
+
+public class FrameOne {
+    
+    public init() {
+        
+    }
+    
+    public func printFrameworkOne() {
+        logMe()
+    }
+}
+
+extension FrameOne {
+    private func logMe() {
+        print("print by Framework One by private method")
+    }
+}
